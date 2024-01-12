b704a016b6cf9c9b804500208e060bbf97da1c65
Saurav Sutaria <saurav.sutaria@tarktech.com>
2024-01-12 10:54:22 +0530

    Problem 4

---
+++ Solutions/PalindromeDecoding.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Text;
+
+namespace Exercise1_Solution.Solutions
+{
+    internal class PalindromeDecoding
+    {
+        public string Decode(String code, int[] position, int[] length)
+        {
+            StringBuilder ans = new StringBuilder(code);
+            for(int i = 0; i < position.Length; i++)
+            {
+                StringBuilder subStr = new StringBuilder(ans.ToString().Substring(position[i], length[i]));
+                //string revString = new string(subStr.ToString().Reverse().ToArray());
+                int k=0,j=subStr.Length-1;
+                while(k < j)
+                {
+                    char temp = (char)subStr[k];
+                    subStr[k] = subStr[j];
+                    subStr[j] = temp;
+                    k++;j--;
+                }
+                ans.Insert(position[i] + length[i],subStr.ToString());
+            }
+            return ans.ToString();
+        }
+    }
+}
