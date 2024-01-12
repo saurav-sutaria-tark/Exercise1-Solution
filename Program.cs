7782d4fdeb43a3a962166d01e22f7a9cf900685b
Saurav Sutaria <saurav.sutaria@tarktech.com>
2024-01-12 10:51:13 +0530

    Till problem 6

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Exercise1_Solution
+﻿using System.Text;
+using Exercise1_Solution.Solutions;
+
+namespace Exercise1_Solution
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //HuffmanDecoding huffmanDecoding = new HuffmanDecoding();
+            //Console.WriteLine(huffmanDecoding.Decode("101101",new string[]{ "00","10","01","11"}));
+            //Console.WriteLine(huffmanDecoding.Decode("10111010", new string[]{ "0","111","10"}));
+            //Console.WriteLine(huffmanDecoding.Decode("0001001100100111001", new string[] { "1", "0" }));
+            //Console.WriteLine(huffmanDecoding.Decode("111011011000100110", new string[] { "010", "00", "0110", "0111", "11", "100", "101" }));
+            //Console.WriteLine(huffmanDecoding.Decode("001101100101100110111101011001011001010", new string[] { "110", "011", "10", "0011", "00011", "111", "00010", "0010", "010", "0000" }));
+
+            //LexmaxReplace lexmaxReplace = new LexmaxReplace();
+            //Console.WriteLine(lexmaxReplace.Get("abb", "c"));
+            //Console.WriteLine(lexmaxReplace.Get("z", "f"));
+            //Console.WriteLine(lexmaxReplace.Get("fedcba", "ee"));
+            //Console.WriteLine(lexmaxReplace.Get("xldyzmsrrwzwaofkcxwehgvtrsximxgdqrhjthkgfucrjdvwlr", "xfpidmmilhdfzypbguentqcojivertdhshstkcysydgcwuwhlk"));
+
+            //SortingSubsets sortingSubsets = new SortingSubsets();
+            //Console.WriteLine(sortingSubsets.GetMinimalSize(new int[]{3,2,1}));
+            //Console.WriteLine(sortingSubsets.GetMinimalSize(new int[] { 1, 2, 3, 4 }));
+            //Console.WriteLine(sortingSubsets.GetMinimalSize(new int[] { 4, 4, 4, 3, 3, 3 }));
+            //Console.WriteLine(sortingSubsets.GetMinimalSize(new int[] { 11, 11, 49, 7, 11, 11, 7, 7, 11, 49, 11 }));
+
+            //PalindromeDecoding palindromeDecoding = new PalindromeDecoding();
+            //Console.WriteLine(palindromeDecoding.Decode("ab", new int[] { 0 }, new int[] { 2 }));
+            //Console.WriteLine(palindromeDecoding.Decode("Misip", new int[] { 2, 3, 1, 7 }, new int[] { 1, 1, 2, 2 }));
+            //Console.WriteLine(palindromeDecoding.Decode("XY", new int[] { 0, 0, 0, 0 }, new int[] { 2, 4, 8, 16 }));
+            //Console.WriteLine(palindromeDecoding.Decode("TC206", new int[] { 1, 2, 5 }, new int[] { 1, 1, 1 }));
+            //Console.WriteLine(palindromeDecoding.Decode("nodecoding", new int[] {}, new int[] { }));
+
+            //MovingAvg movingAvg = new MovingAvg();
+            //Console.WriteLine(movingAvg.Difference(2, new double[] { 3, 8, 9, 15 }));
+            //Console.WriteLine(movingAvg.Difference(3, new double[] { 17, 6.2, 19, 3.4 }));
+            //Console.WriteLine(movingAvg.Difference(3, new double[] { 6, 2.5, 3.5 }));
+
+            WordCompositionGame game = new WordCompositionGame();
+            Console.WriteLine(game.Score(new string[] { "cat", "dog", "pig", "mouse" }, new string[] { "cat", "pig" }, new string[] { "dog", "cat" }));
+            Console.WriteLine(game.Score(new string[] { "mouse" }, new string[] { "cat", "pig" }, new string[] { "dog", "cat" }));
+            Console.WriteLine(game.Score(new string[] { "dog", "mouse" }, new string[] { "dog", "pig" }, new string[] { "dog", "cat" }));
+            Console.WriteLine(game.Score(new string[] { "bcdbb", "aaccd", "dacbc", "bcbda", "cdedc", "bbaaa", "aecae" }, new string[] { "bcdbb", "ddacb", "aaccd", "adcab", "edbee", "aecae", "bcbda" }, new string[] {"dcaab","aadbe","bbaaa","ebeec","eaecb","bcbba","aecae","adcab","bcbda"}));
+
         }
+
     }
 }
