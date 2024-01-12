775cc45d772651e14c1e63e310113d8a54eb0267
Saurav Sutaria <saurav.sutaria@tarktech.com>
2024-01-12 10:53:52 +0530

    Problem 2

---
+++ Solutions/LexmaxReplace.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text;
+
+namespace Exercise1_Solution.Solutions
+{
+    internal class LexmaxReplace
+    {
+        public StringBuilder Get(String s, String t)
+        {
+            char[] ch = t.ToCharArray();
+            Array.Sort(ch);
+            StringBuilder ans = new StringBuilder(s);
+            int i=0, j = ch.Length-1;
+            while(i < s.Length && j >= 0)
+            {
+                if (ans[i] < ch[j])
+                {
+                    ans[i] = ch[j];
+                    j--;
+                }
+                i++;
+            }
+            return ans; 
+        }
+    }
+}
